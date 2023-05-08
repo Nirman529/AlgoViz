04572a5decedc37eeafe2a7f8b50b7bc6d858a3b
Nirman529 <68377229+Nirman529@users.noreply.github.com>
2023-05-08 10:57:16 +0530

    Folder restructure and adding components

---
+++ algo-viz/src/components/AlgoViz.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const AlgoViz = () => {
+    const head = () => {};
+
+    return (
+        <>
+            <h1>Welcome to AlgoViz</h1>
+        </>
+    );
+};
+
+export default AlgoViz;
