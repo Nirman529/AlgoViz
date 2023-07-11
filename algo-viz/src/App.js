b39bc924d7888471e4fe6217976618e7c7587762
Nirman529 <68377229+Nirman529@users.noreply.github.com>
2023-07-11 22:38:02 +0530

    remove old code and reformat the structure of project

--- algo-viz/src/App.js
+++ algo-viz/src/App.js
@@ -1,16 +1,31 @@
 import 'bootstrap/dist/css/bootstrap.css';
 import "./App.css";
-
 import React from "react";
-import AlgoViz from "./components/AlgoViz";
+import { BrowserRouter, Routes, Route, NavLink } from "react-router-dom"
 import Grid from './components/Grid/Grid';
+import PathFinder from './components/PathFinder/PathFinder';
+import Home from './components/Home/Home';
+import Layout from './components/Layout/Layout';
+import PageNotFound from './components/NoPage/PageNotFound';
 
 function App() {
     return (
         <>
             <div className="App">
-                <AlgoViz />
-                <Grid />
+
+                <BrowserRouter>
+                    <Routes>
+                        <Route path='/' element={<Layout />}>
+                            <Route index element={<Home />} />
+                            <Route path='aStarPathFinder' element={<Grid />} />
+                            <Route path='*' element={<PageNotFound />} />
+
+                        </Route>
+                    </Routes>
+                </BrowserRouter>
+
+
+
             </div>
         </>
     );
--- algo-viz/src/components/Grid/Grid.jsx
+++ algo-viz/src/components/Grid/Grid.jsx
@@ -1,94 +1,9 @@
-import React, { useEffect, useState } from 'react'
-import Node from '../Node'
-
-
-const rows = 10;
-const cols = 10;
-
-const startNode = [0, 0];
-const endNode = [rows - 1, cols - 1];
+import React from 'react'
 
 const Grid = () => {
-    const [grid, setGrid] = useState([]);
-    const [path, setPath] = useState([]);
-    const [visitedNode, setVisitedNode] = useState([]);
-
-    useEffect(() => {
-
-        const grid = new Array(rows);
-        for (let i = 0; i < rows; i++) {
-            grid[i] = new Array(cols);
-        }
-    }, []);
-
-    const makeNeighborForAll = (grid) => {
-        for (let i = 0; i < rows; i++) {
-            for (let j = 0; j < cols; j++) {
-                grid[i][j].addNeighbors(grid);
-            }
-        }
-    }
-
-    // grid with node
-    const gridWithNode = (
-        <div>
-            {grid.map((row, rowIndex) => {
-                return (
-                    <div key={rowIndex} className="grid-row">
-                        {row.map((col, colIndex) => {
-                            const { isStartNode, isEndNode, isObstacle } = col;
-                            return (
-                                <Node
-                                    key={colIndex}
-                                    isStartNode={isStartNode}
-                                    isEndNode={isEndNode}
-                                    row={rowIndex}
-                                    col={colIndex}
-                                    isObstacle={isObstacle}
-                                />
-                            );
-                        })}
-                    </div>
-                );
-            })}
-        </div>
-    );
-
-    const visualizeShortestPath = (shortestPathNodes) => {
-        for (let i = 0; i < shortestPathNodes.length; i++) {
-            setTimeout(() => {
-                const node = shortestPathNodes[i];
-                console.log(node);
-                document.getElementById(`node-${node.x}-${node.y}`).className = "node node-shortest-path";
-            }, i * 10);
-        }
-    }
-    const visualisePath = () => {
-        for (let i = 0; i <= visitedNode.length; i++) {
-            if (i === visitedNode.length) {
-                setTimeout(() => {
-                    visualizeShortestPath(path);
-                }, i * 20);
-            } else {
-                setTimeout(() => {
-                    const node = visitedNode[i];
-                    console.log(node);
-                    document.getElementById(`node-${node.x}-${node.y}`).className = "node node-visited";
-                }, 20 * i);
-
-            }
-        }
-
-    }
-    console.log(path);
-
-
-    return (
-        <div className="Wrapper">
-            <button onClick={visualisePath}>Visualise Path</button>
-            {gridWithNode}
-        </div>
-    )
+  return (
+    <div>Grid</div>
+  )
 }
 
 export default Grid
