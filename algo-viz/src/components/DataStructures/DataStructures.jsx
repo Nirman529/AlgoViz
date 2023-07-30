3eaec07adab5c31a3525f7691cefb35029bad212
Nirman529 <68377229+Nirman529@users.noreply.github.com>
2023-07-30 22:25:48 +0530

    changes in UI

--- algo-viz/src/components/DataStructures/DataStructures.jsx
+++ algo-viz/src/components/DataStructures/DataStructures.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
 
+
 const DataStructures = () => {
   return (
-    <div>DataStructures</div>
+    <>
+      <div className='row m-0'>
+        <div className='col bg-light border p-1 m-1'>
+
+        DataStructures
+        </div>
+        <div className='col bg-light border p-1 ms-0 m-1'>options</div>
+      </div>
+    </>
   )
 }
 
